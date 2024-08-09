aa3cf8cb19b5d92569fd26fd0e71fa1b328f8ff0
n129bz <n129bz@outlook.com>
2024-08-09 13:24:00 -0500

    Added individual VFR area chart processing capability

--- make.js
+++ make.js
@@ -31,6 +31,7 @@ class ChartProcessTime {
 
 const fs = require('fs');
 const { execSync, exec } = require('child_process');
+const prompt = require('prompt-sync')({sigint: true});
 
 // load settings
 const appdir = process.cwd();
@@ -85,43 +86,87 @@ let isifrchart = false;
 /**
  * Chart processing starts here
  */
-settings.chartprocessindexes.forEach((index) => {
-    chartworkname = settings.faachartnames[index][0];
-    chartlayertype = settings.layertypes[settings.layertypeindex];
-    isifrchart = settings.faachartnames[index][1] === "ifr";
-    if (isifrchart) {
-        // IFR chart
-        charturl = settings.ifrdownloadtemplate.replace("<chartdate>", chartdate).replace("<charttype>", chartworkname);
-        clippedShapeFolder = `${appdir}/clipshapes/${settings.faachartnames[index][2].toLowerCase()}`;
-        chartname = settings.faachartnames[index][2]; // use alias value for IFR
-        chartfolder = `${workarea}/${chartname}`;
+let resp = prompt("Enter 0 to process all charts in the chartprocessindexes array, or 1 to process a single VFR chart: "); 
+let nm = Number(resp);
+if (nm === 1) {
+    let lst = "\nSelect the chart number you want to process from this list\n\n";
+    for (var i = 0; i <  settings.vfrindividualcharts.length; i++) {
+        lst += `${settings.vfrindividualcharts[i][0]} ${settings.vfrindividualcharts[i][1]}\n`; 
+    }
+    lst += "\n";
+    resp = prompt(lst);
+    nm = Number(resp); 
+    if (nm >= 1 && nm <= 53) {
+        chartworkname = settings.vfrindividualcharts[nm - 1][1];
+        chartname = chartworkname;
+        clippedShapeFolder = `${appdir}/clipshapes/sectional`;
+        chartlayertype = settings.layertypes[settings.layertypeindex];
+        chartfolder = `${workarea}/${chartworkname}`;
+        charturl = `${settings.vfrindividualtemplate.replace("<chartdate>", chartdate).replace("<charttype>", chartworkname)}`;
+        console.log(charturl);
+
+        let cpt = new ChartProcessTime(chartname);
+        timings.set(chartname, cpt);
+
+        dir_1_unzipped = `${chartfolder}/1_unzipped`;
+        dir_2_expanded = `${chartfolder}/2_expanded`;
+        dir_3_clipped = `${chartfolder}/3_clipped`;
+        dir_4_tiled = `${chartfolder}/4_tiled`;
+        dir_5_merged = `${chartfolder}/5_merged`;
+        dir_6_quantized = `${chartfolder}/6_quantized`;
+        
+        setupEnvironment();
+        downloadSingleChart(charturl);
+        unzipCharts();
+        normalizeChartNames();
+        processImages();
+        mergeAndQuantize();
+        makeMbTiles();
     }
     else {
-        // VFR chart
-        charturl = settings.vfrdownloadtemplate.replace("<chartdate>", chartdate).replace("<charttype>", chartworkname);
-        clippedShapeFolder = `${appdir}/clipshapes/${chartworkname.toLowerCase()}`;
-        chartname = chartworkname;
-        chartfolder = `${workarea}/${chartworkname}`;
+        prompt("Invalid response, exiting!");
+        process.exit();
     }
-    
-    let cpt = new ChartProcessTime(chartname);
-    timings.set(chartname, cpt);
+}
+else {
+    settings.chartprocessindexes.forEach((index) => {
+        chartworkname = settings.faachartnames[index][0];
+        chartlayertype = settings.layertypes[settings.layertypeindex];
+        isifrchart = settings.faachartnames[index][1] === "ifr";
+        if (isifrchart) {
+            // IFR chart
+            charturl = settings.ifrdownloadtemplate.replace("<chartdate>", chartdate).replace("<charttype>", chartworkname);
+            clippedShapeFolder = `${appdir}/clipshapes/${settings.faachartnames[index][2].toLowerCase()}`;
+            chartname = settings.faachartnames[index][2]; // use alias value for IFR
+            chartfolder = `${workarea}/${chartname}`;
+        }
+        else {
+            // VFR chart
+            charturl = settings.vfrdownloadtemplate.replace("<chartdate>", chartdate).replace("<charttype>", chartworkname);
+            clippedShapeFolder = `${appdir}/clipshapes/${chartworkname.toLowerCase()}`;
+            chartname = chartworkname;
+            chartfolder = `${workarea}/${chartworkname}`;
+        }
+        
+        let cpt = new ChartProcessTime(chartname);
+        timings.set(chartname, cpt);
 
-    dir_1_unzipped = `${chartfolder}/1_unzipped`;
-    dir_2_expanded = `${chartfolder}/2_expanded`;
-    dir_3_clipped = `${chartfolder}/3_clipped`;
-    dir_4_tiled = `${chartfolder}/4_tiled`;
-    dir_5_merged = `${chartfolder}/5_merged`;
-    dir_6_quantized = `${chartfolder}/6_quantized`;
-    
-    setupEnvironment();
-    downloadCharts();
-    unzipCharts();
-    normalizeChartNames();
-    processImages();
-    mergeAndQuantize();
-    makeMbTiles();
-});
+        dir_1_unzipped = `${chartfolder}/1_unzipped`;
+        dir_2_expanded = `${chartfolder}/2_expanded`;
+        dir_3_clipped = `${chartfolder}/3_clipped`;
+        dir_4_tiled = `${chartfolder}/4_tiled`;
+        dir_5_merged = `${chartfolder}/5_merged`;
+        dir_6_quantized = `${chartfolder}/6_quantized`;
+        
+        setupEnvironment();
+        downloadCharts();
+        unzipCharts();
+        normalizeChartNames();
+        processImages();
+        mergeAndQuantize();
+        makeMbTiles();
+    });
+}
 
 timings.forEach((cpt, ckey) => {
     console.log(`${cpt.totaltime}\n`);
@@ -195,15 +240,38 @@ function unzipCharts() {
                 executeCommand(cmd);
             }
         });
+    }
 
-        // now clean out all of the irrelevant files...
-        cmd = `rm -r -f ${dir_1_unzipped}/*.pdf`;
-        executeCommand(cmd);
+    // now clean out all of the irrelevant files...
+    cmd = `rm -r -f ${dir_1_unzipped}/*.pdf`;
+    executeCommand(cmd);
 
-        cmd = `rm -r -f ${dir_1_unzipped}/*.htm`;
-        executeCommand(cmd);
+    cmd = `rm -r -f ${dir_1_unzipped}/*.htm`;
+    executeCommand(cmd);
 
-        cmd = `rm -r -f ${dir_1_unzipped}/*.zip`;
+    cmd = `rm -r -f ${dir_1_unzipped}/*.zip`;
+    executeCommand(cmd);
+}
+
+/**
+ * Get the single chart zip file from the FAA's digital sources URL
+ */
+function downloadSingleChart(singlecharturl) {
+    let chartzip = `${chartcache}/${chartworkname}-${chartdate}.zip`;
+    if (fs.existsSync(chartzip)) {
+        logEntry(`Using cached ${chartzip}`);
+        return;
+    }
+    else {
+        let oldfiles = fs.readdirSync(chartcache);
+        for (var i = 0; i < oldfiles.length; i++) {
+            if (oldfiles[i].startsWith(chartworkname)) {
+                fs.rmSync(`${chartcache}/${oldfiles[i]}`);
+                break;
+            }
+        }
+        logEntry(`Downloading ${chartzip}`);
+        cmd = `curl ${singlecharturl} -o ${chartzip}`;
         executeCommand(cmd);
     }
 }
