09139f5ff1828a1534ed491af1282ede0634b40c
n129bz <n129bz@outlook.com>
2024-09-24 22:43:52 -0500

    Added cutlineblendpixels to processImages() function

--- make.js
+++ make.js
@@ -464,6 +464,7 @@ function processImages() {
 
     let chartareas = buildChartNameArray();
     let processes = getProcessCount();
+    let cblend = settings.cutlineblendpixels;
 
     chartareas.forEach((area) => {
         logEntry(`* chart ${area}`);
@@ -486,7 +487,7 @@ function processImages() {
         executeCommand(cmd);
 
         logEntry(`>> gdalwarp warping and clipping ${clipped} using shapefile ${shapefile}`);
-        cmd = `gdalwarp -t_srs EPSG:3857 -dstalpha --config GDAL_CACHEMAX 256 -co SKIP_NOSOURCE=YES -multi -cblend 8 -cutline ${shapefile} -crop_to_cutline ${expanded} ${clipped}`;
+        cmd = `gdalwarp -t_srs EPSG:3857 -dstalpha --config GDAL_CACHEMAX 256 -co SKIP_NOSOURCE=YES -multi -cblend ${cutlineblendpixels} -cutline ${shapefile} -crop_to_cutline ${expanded} ${clipped}`;
         executeCommand(cmd);
 
         // setup formatting arguments for overviews        
